84c2ca089a56f076a711df0a662807a860619848
EC2 Default User <ec2-user@ip-172-31-1-95.ap-northeast-2.compute.internal>
2019-12-01 17:49:52 +0000

    Secondcommit

--- Hello.java
+++ Hello.java
@@ -1,5 +1,7 @@
 public class Hello {
         public static void main(String[] args) {
                 System.out.println("Hello, workld from Git!");
+		System.out.println("Hello, Commit-2");
+
 	}
 }
