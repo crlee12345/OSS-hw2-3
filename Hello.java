de669517db35236a524452b7f911fb218527d854
EC2 Default User <ec2-user@ip-172-31-1-95.ap-northeast-2.compute.internal>
2019-12-01 17:52:01 +0000

    Thirdcommit

--- Hello.java
+++ Hello.java
@@ -2,6 +2,7 @@ public class Hello {
         public static void main(String[] args) {
                 System.out.println("Hello, workld from Git!");
 		System.out.println("Hello, Commit-2");
+		System.out.println("Hello, Commit-3");
 
 	}
 }
